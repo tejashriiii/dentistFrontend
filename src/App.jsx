6a8f05dcc36bdaf88c3c4f8dfb5316ed2059d732
tejashriiii <masketejashri11@gmail.com>
2025-01-28 11:35:07 +0530

    Added tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import PatientRegisterForm from './PatientRegisterForm';
+import PatientRegisterForm from './components/PatientRegisterForm';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-100 bgflex items-center justify-center">
       <PatientRegisterForm />
     </div>
   );
