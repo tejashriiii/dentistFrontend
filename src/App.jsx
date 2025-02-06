776a65dc2ae6230d767e5d41ef9af7c966357427
blacklytning <blacklytning@yandex.com>
2025-02-06 19:57:50 +0530

    Fully add role-based authentication

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,16 @@ import PatientAppointmentForm from "./components/PatientAppointmentForm";
 import FollowUp from "./components/FollowUp";
 import Home from "./pages/Home";
 import DailyTreatment from "./pages/DailyTreatment";
+import ProtectedRoute from './components/ProtectedRoute';
+import Unauthorized from "./pages/Unauthorized";
 
 const App = () => {
   const [patients, setPatients] = React.useState([]);
-  const [xrayPrices, setXrayPrices] = React.useState({
-    1: 50,
-    2: 60,
-    3: 70, // Default X-ray prices
-  });
-
-  const isAdmin = true; // Change this based on user role (main admin/staff)
-
-  // Function to update X-ray price (Admins only)
-  const updatePrice = (tooth, price) => {
-    setXrayPrices((prev) => ({ ...prev, [tooth]: Number(price) }));
-  };
 
   const addPatient = (newPatient) => {
     setPatients([...patients, newPatient]);
   };
-
+  
   return (
     <Router>
       <div>
@@ -36,12 +26,12 @@ const App = () => {
             <Link to="/" className="mr-4 ">
               Admin Dashboard
             </Link>
-            <Link to="/Home" className="p-2">
+             <Link to="/Home" className="p-2">
               HomePage
             </Link>
-            <Link to="/dailytreat" className="p-2">
-              docDailyTreatment
-            </Link>
+              <Link to="/dailytreat" className="p-2">
+                docDailyTreatment
+              </Link>
           </div>
           <div>
             <Link to="/register" className="p-2">
@@ -65,10 +55,9 @@ const App = () => {
 
         <Routes>
           <Route path="/" element={<ReceptionDashboard patients={patients} />} />
-          <Route
-            path="/register"
-            element={<PatientRegisterForm addPatient={addPatient} />}
-          />
+          <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+            <Route path="/register" element={<PatientRegisterForm addPatient={addPatient} />} />
+          </Route>
           <Route
             path="/login"
             element={<CredentialsForm formAction="login" />}
@@ -80,8 +69,10 @@ const App = () => {
           <Route path="/appointment" element={<PatientAppointmentForm />} />
           <Route path="/followUp" element={<FollowUp userType="doctor" />} />
           <Route path="/Home" element={<Home />} />
-          <Route path="/dailytreat" element={<DailyTreatment />} />
-
+          <Route element={<ProtectedRoute allowedRoles={["Doctor"]} />}>
+            <Route path="/dailytreat" element={<DailyTreatment />} />
+          </Route>
+          <Route path="/unauthorized" element={<Unauthorized />} />
         </Routes>
       </div>
     </Router>
@@ -89,3 +80,97 @@ const App = () => {
 };
 
 export default App;
+
+
+// Old - DONT DELETE THIS CODE App.jsx
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+// import ReceptionDashboard from "./components/ReceptionDashboard";
+// import PatientRegisterForm from "./components/PatientRegisterForm";
+// import CredentialsForm from "./components/CredentialsForm";
+// import PatientAppointmentForm from "./components/PatientAppointmentForm";
+// import FollowUp from "./components/FollowUp";
+// import Home from "./pages/Home";
+// import DailyTreatment from "./pages/DailyTreatment";
+
+// const App = () => {
+//   const [patients, setPatients] = React.useState([]);
+//   const [xrayPrices, setXrayPrices] = React.useState({
+//     1: 50,
+//     2: 60,
+//     3: 70, // Default X-ray prices
+//   });
+
+//   const isAdmin = true; // Change this based on user role (main admin/staff)
+
+//   // Function to update X-ray price (Admins only)
+//   const updatePrice = (tooth, price) => {
+//     setXrayPrices((prev) => ({ ...prev, [tooth]: Number(price) }));
+//   };
+
+//   const addPatient = (newPatient) => {
+//     setPatients([...patients, newPatient]);
+//   };
+
+//   return (
+//     <Router>
+//       <div>
+//         <nav className="bg-[var(--darkgreen)] p-4 flex justify-between ">
+//           <div>
+//             <Link to="/" className="mr-4 ">
+//               Admin Dashboard
+//             </Link>
+//             <Link to="/Home" className="p-2">
+//               HomePage
+//             </Link>
+//             <Link to="/dailytreat" className="p-2">
+//               docDailyTreatment
+//             </Link>
+//           </div>
+//           <div>
+//             <Link to="/register" className="p-2">
+//               Register Patient
+//             </Link>
+//             <Link to="/login" className="p-2">
+//               Login
+//             </Link>
+//             <Link to="/signup" className="p-2">
+//               Signup
+//             </Link>
+//             <Link to="/appointment" className="p-2">
+//               Appointment
+//             </Link>
+//             <Link to="/followup" className="p-2">
+//               followup
+//             </Link>
+
+//           </div>
+//         </nav>
+
+//         <Routes>
+//           <Route path="/" element={<ReceptionDashboard patients={patients} />} />
+//           <Route
+//             path="/register"
+//             element={<PatientRegisterForm addPatient={addPatient} />}
+//           />
+//           <Route
+//             path="/login"
+//             element={<CredentialsForm formAction="login" />}
+//           />
+//           <Route
+//             path="/signup"
+//             element={<CredentialsForm formAction="signup" />}
+//           />
+//           <Route path="/appointment" element={<PatientAppointmentForm />} />
+//           <Route path="/followUp" element={<FollowUp userType="doctor" />} />
+//           <Route path="/Home" element={<Home />} />
+//           <Route path="/dailytreat" element={<DailyTreatment />} />
+
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+// DONT DELETE THIS CODE 
